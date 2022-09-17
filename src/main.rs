722fc1574e8fa2f4a57a4711768407a162ce3ece
Hamidreza khosroabadi <hamidrezakhosroabadi@outlook.com>
2022-09-17 17:57:15 +0430

    - First bootable image is ready!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-#![no_std] 
-#![no_main] 
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
-#[no_mangle] 
+static HI: &[u8] = b"Hello World!";
+
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HI.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
