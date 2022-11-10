6b180c0246e821fc595c843e847068656b0375fc
Administrator <administrator@localhost.localdomain>
2022-11-10 14:32:45 +0330

    - bootstrap.c was linted!

--- bootstrap/bootstrap.c
+++ bootstrap/bootstrap.c
@@ -2,30 +2,35 @@ typedef unsigned short u16;
 
 int tsize, dsize, ksectors, i, NSEC = 1;
 
-int prints(char *s){ 
-  while(*s) putc(*s++);
+int prints(char *s)
+{
+  while (*s)
+    putc(*s++);
 }
 
-int getsector(u16 sector){
-  readfd(sector/36,((sector)%36)/18,(((sector)%36)%18));  
+int getsector(u16 sector)
+{
+  readfd(sector / 36, ((sector) % 36) / 18, (((sector) % 36) % 18));
 }
 
-main(){
+main()
+{
   prints("booting kernel!\n\r");
-  
-  tsize = *(int *)(512+2);
-  dsize = *(int *)(512+4);
-  ksectors = ((tsize << 4) + dsize + 511)/512;
-  
+
+  tsize = *(int *)(512 + 2);
+  dsize = *(int *)(512 + 4);
+  ksectors = ((tsize << 4) + dsize + 511) / 512;
+
   setes(0x1000);
-  
-  for(i=1; i<=ksectors+1; i++){
+
+  for (i = 1; i <= ksectors + 1; i++)
+  {
     getsector(i);
     inces();
     putc('.');
   }
-  
+
   prints("\n\rready to go?");
-  
+
   getc();
 }
